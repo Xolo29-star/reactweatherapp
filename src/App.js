2c69f90b2b8b927249a04d4ca15f715105349f31
Xolo29-star <145863760+Xolo29-star@users.noreply.github.com>
2024-04-03 19:02:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
     
         <Weather defaultCity="New York" />
         <footer>
-          This page was coded by <a href="https://github.com/Xolo29-star" target="_blank" rel="noreferrer">Noxolo Gama</a> and open-sourced on <a href="https://github.com/Xolo29-star/reactweatherapp/tree/master" target="_blank" rel="noreferrer">Github</a>
+          This page was coded by <a href="https://github.com/Xolo29-star" target="_blank" rel="noreferrer">Noxolo Gama</a> and open-sourced on <a href="https://github.com/Xolo29-star/reactweatherapp/tree/master" target="_blank" rel="noreferrer">Github</a> and hosted on <a href="https://ornate-yeot-4ecbb2.netlify.app/" target="_blank" rel="noreferrer">netlify</a>
+        </footer>
+      </div>
         </footer>
       </div>
     </div>
